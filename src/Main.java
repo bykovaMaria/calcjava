302c7619298f7c0d068047587dab6647d1addf44
Maria test <bykova1985maria@gmail.com>
2023-11-24 20:12:01 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+      int amount = 10500;
+      int milli = 20;
+      int bonus = amount / milli;
+
+      System.out.println("Итоговый бонус: " + bonus);
+
+
+
+
+    }
+}
